2c30c204519fee9205a1efeb928803a0ce99921b
sakuya3834 <sakuya3834@gmail.com>
2015-05-02 10:25:40 -0700

    fixed quicksort

--- quicksort.c
+++ quicksort.c
@@ -1,40 +1,37 @@
 #include "quicksort.h"
 
-void quicksort(int *numbers, int start, int end)
+void quicksort(int *numbers, int left, int right)
 {
-	int left = 0;
-	int right = 0;
-	int pivot = 0;
-	int temp = 0;
+	int i = 0;
+	int *last;
 
-	if (start < end)
+	if (right <= left)
 	{
-		left = start;
-		right = end;
-		pivot = start;
-
-		while (left <= right)
-		{
-			while (*(numbers + left) < *(numbers + pivot))
-			{
-				left++;
-			}
-
-			while (*(numbers + right) > *(numbers + pivot))
-			{
-				right--;
-			}
-
-			if (left <= right)
-			{
-				temp = *(numbers + left);
-				*(numbers + left) = *(numbers + right);
-				*(numbers + right) = temp;
-				left++;
-				right--;
-			}
-		}
-		quicksort(numbers, start, right);
-		quicksort(numbers, left, end);
+		return;
 	}
+	
+	last = numbers + left; 
+
+	for (i = left + 1; i < right; i++)
+	{
+		if (*(numbers + i) < *(numbers + left))
+		{
+			int temp;
+
+			last++;
+
+			temp = *last;
+			*last = *(numbers + i);
+			*(numbers + i) = temp;
+		}
+	}
+
+	int temp;
+
+	temp = *(numbers + left);
+	*(numbers + left) = *last;
+	*last = temp;
+
+	quicksort(numbers, left, last - numbers);
+	quicksort(numbers, last - numbers + 1, right);
 }
