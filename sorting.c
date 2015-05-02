394a9b1c1ee68d9fb60040500cc2cff87cf53a8d
sakuya3834 <sakuya3834@gmail.com>
2015-05-02 10:29:07 -0700

    added options for command line testing

--- sorting.c
+++ sorting.c
@@ -8,9 +8,12 @@ int main()
 {
 	int n = 0;
 	int i = 0;
+	int choice = 0;
 
 	// ask user for input
-	printf("Enter the number of elements to sort: ");
+	printf("Please enter 1 for quicksort or 2 for bubblesort:");
+	scanf_s("%d", &choice);
+	printf("\nEnter the number of elements to sort: ");
 	scanf_s("%d", &n);
 	printf("Generating %d numbers...", n);
 
@@ -22,7 +25,14 @@ int main()
 		*(randomNumbers + i) = rand();
 	}
 
-	quicksort(randomNumbers, 0, n);
+	if (choice == 1)
+	{
+		quicksort(randomNumbers, 0, n);
+	}
+	else if (choice == 2)
+	{
+		bubblesort(randomNumbers, n);
+	}
 
 	for (i = 0; i < n; i++)
 	{
