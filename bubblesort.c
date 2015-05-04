f8ea8a5908a6c699ec9846023360d63fd489e761
sakuya3834 <sakuya3834@gmail.com>
2015-05-03 19:29:47 -0700

    test final linux version

--- bubblesort.c
+++ bubblesort.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<time.h>
 #include "bubblesort.h"
 
 void bubblesort(int *numbers, int n)
@@ -18,4 +20,27 @@ void bubblesort(int *numbers, int n)
 			}
 		}
 	}
+}
+
+int main(int argc, char *argv[])
+{
+	int n = atoi(argv[1]);
+	int i = 0;
+
+	// generate n random numbers where n is based on user input
+	srand(time(NULL));
+	int *randomNumbers = malloc(sizeof(int)*n);
+	for (i = 0; i < n; i++)
+	{
+		*(randomNumbers + i) = rand();
+	}
+
+	bubblesort(randomNumbers, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d\n", *(randomNumbers + i));
+	}
+
+	return 0;
 }
--- quicksort.c
+++ quicksort.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<time.h>
 #include "quicksort.h"
 
 void quicksort(int *numbers, int left, int right)
@@ -34,4 +36,29 @@ void quicksort(int *numbers, int left, int right)
 
 	quicksort(numbers, left, last - numbers);
 	quicksort(numbers, last - numbers + 1, right);
+}
+
+int main(int argc, char *argv[])
+{
+	int n = atoi(argv[1]);
+	int i = 0;
+
+	// generate n random numbers where n is based on user input
+	srand(time(NULL));
+	int *randomNumbers = malloc(sizeof(int)*n);
+	for (i = 0; i < n; i++)
+	{
+		*(randomNumbers + i) = rand();
+	}
+
+	
+	quicksort(randomNumbers, 0, n);
+	
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d\n", *(randomNumbers + i));
+	}
+
+	return 0;
 }
